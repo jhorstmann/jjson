96bb514a0d3498ea626841fff3ebb25228da36b0
Jörn Horstmann <git@jhorstmann.net>
2012-04-10 23:40:29 +0200

    Test for JSONArray iteration

---
+++ src/test/java/net/jhorstmann/json/JSONArrayTest.java
@@ -0,0 +1,23 @@
+package net.jhorstmann.json;
+
+import static junit.framework.Assert.*;
+import org.junit.Test;
+
+public class JSONArrayTest {
+    @Test
+    public void testArray() {
+        JSONArray arr = new JSONArray();
+        arr.add(1.0);
+        arr.add("abc");
+        arr.add(Boolean.TRUE);
+        arr.add(JSONNull.INSTANCE);
+        JSONListIterator iter = arr.jsonListIterator();
+        assertTrue(iter.hasNext());
+        assertEquals(1.0, iter.nextDouble());
+        assertEquals("abc", iter.nextString());
+        assertTrue(iter.nextBoolean());
+        assertTrue(iter.nextIsNull());
+        assertFalse(iter.hasNext());
+    }
+
+}
